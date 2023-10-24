40222b40ae496ecce32d11565fac393952d803b7
Apu Das <74287939+apu-das@users.noreply.github.com>
2023-10-25 02:09:41 +0600

    Number_Guessing_Game.py

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -1,7 +1,7 @@
 
 ####################### Description of the project #####################################
  # Build a Number guessing game, in which the user selects a range.
- # Let’s say User selected a range, i.e., from X to Y, where X and Y belong to Integer.
+ # Let’s say the User selected a range, i.e., from X to Y, where X and Y belong to Integer.
  # Some random integer will be selected by the system and the user has to guess that integer in the minimum number of guesses
 
 ########################################################################################
@@ -9,16 +9,16 @@
 import random
 import math
 
-# Taking input from user
+# Taking input from the user
 lower = int(input('Enter lower bound: '))
 
-# Taking input from user
+# Taking input from the user
 upper = int(input('Enter upper bound: '))
 
 # Generating numbers between lower and upper bound
 x = random.randint(lower, upper)
 
-# Calculating minimum number of tries
+# Calculating the minimum number of tries
 # Minimum number of guessing = log2(Upper bound – lower bound + 1)
 y = round(math.log(upper - lower + 1, 2))
 print("\n You've only ", y, " chances to guess the number! \n")
@@ -36,7 +36,7 @@ while count < y:
     # Checking condition
     if guess == x:
         print('Congratulations you did it in ', count, 'times !')
-        # Once the condition satisfies, the loop will break
+        # Once the condition satisfied, the loop will break
         break
     elif guess < x:
         print('You guessed too small !')
